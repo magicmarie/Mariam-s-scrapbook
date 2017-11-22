9c3ed28546e459a0ee8a3d4485418be41498c8a3
magicmarie <31403932+magicmarie@users.noreply.github.com>
2017-11-22 16:33:45 +0300

    Added a function that returns user input

--- workwtetwin.py
+++ workwtetwin.py
@@ -3,3 +3,14 @@ print (name)
 
 num = 12
 print (num)
+
+name1 = str(input("Enter name:"))
+age = int(input("Enter age"))
+#print ("Hey" + name1) 
+
+#function that takes in user input and then greet the user with input
+def read_input(name, age):
+    """ reads user input"""
+    return "hey, {}  {} old".format(name, age)
+
+print (read_input(name1, age))
