a0f0d08569285e3536b12e9c74eb6d2ffbfb5581
magicmarie <31403932+magicmarie@users.noreply.github.com>
2017-11-22 16:28:31 +0300

    Added tests for user input and function

--- tests.py
+++ tests.py
@@ -6,7 +6,9 @@ class workwtetwinTestCase(unittest.TestCase):
     
     def setUp(self):
         """initialisation """
-        pass
+        self.name1 = workwtetwin.name1
+        self.age = workwtetwin.age
+    
 
     def test_name(self):
         """ tests if name mariam is printed"""
@@ -18,6 +20,18 @@ class workwtetwinTestCase(unittest.TestCase):
         num = workwtetwin.num
         self.assertEqual(num, 12)
 
+    def test_user_name_age(self):
+        """test for user name """
+       
+        self.assertIsInstance(self.name1, str)
+        self.assertIsInstance(self.age, int)
+        
+    def test_function(self):
+        """ test read input function"""
+        greet = workwtetwin.read_input(self.name1, self.age)
+        self.assertEqual ("hey, {}  {} old".format(self.name1, self.age), greet)
+        
+        
         
         
 if __name__ == "__main__":
