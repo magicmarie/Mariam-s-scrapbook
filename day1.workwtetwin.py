081c5d4a2083c9875afc65a63b2bf6879fe58d3b
magicmarie <31403932+magicmarie@users.noreply.github.com>
2017-12-05 17:50:10 +0300

    Changed filename

---
+++ day1.workwtetwin.py
@@ -0,0 +1,19 @@
+name = 'mariam'
+print(name)
+
+num = 12
+print(num)
+
+name1 = str(input("Enter name:"))
+age = int(input("Enter age"))
+#print ("Hey" + name1)
+
+# function that takes in user input and then greet the user with input
+
+
+def read_input(names, age):
+    """ reads user input"""
+    return "hey, {} {} old".format(name1, age)
+
+
+print(read_input(name1, age))
